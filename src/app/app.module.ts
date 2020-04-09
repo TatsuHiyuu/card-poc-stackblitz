eda02ba60508d9e57f38259bd1c3c3e9cb8a4a5d
TatsuHiyuu <tatsu.hiyuu@gmail.com>
2020-04-09 17:53:57 +0200

    1 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { CardComponent } from './card/card.component';
+import { BoardComponent } from './board/board.component';
+import { AreaComponent } from './area/area.component';
+import { HandComponent } from './hand/hand.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, CardComponent, BoardComponent, AreaComponent, HandComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }

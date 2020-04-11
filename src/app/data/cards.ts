d62ce818f45c1d9919b837f034ed536510104d67
TatsuHiyuu <tatsu.hiyuu@gmail.com>
2020-04-11 17:29:37 +0200

    4 files modified

--- src/app/data/cards.ts
+++ src/app/data/cards.ts
@@ -5,7 +5,7 @@ export const cards = [
     might: 376,
     description: 'Known as the "Thunderswift Lord" for his ability to wield thunder, he also leads the Knights of Levin. He\'s on a quest to find the seeds of the abyss that nearly decimated his homeland. His calm determination makes him respected by all.',
     image: "https://gamepress.gg/dragalialost/sites/dragalialost/files/2019-02/Albert-5-star.png",
-    position: "-22em -14em",
+    position: "-22em -12em",
     theme: "red-theme"
   },
   {
@@ -14,7 +14,7 @@ export const cards = [
     might: 280,
     description: 'A woman who trains nobles in the way of the blade. Though thought of as a kind older sister, her ferocity is unleashed once lessons begin. But that\'s just another side of her sisterly love, as she desires her students to become strong.',
     image: "https://gamepress.gg/dragalialost/sites/dragalialost/files/2018-10/Celliera-5-star_0.png",
-    position: "-30em -8em",
+    position: "-30em -6em",
     theme: "blue-theme"
   },
   {
@@ -23,7 +23,16 @@ export const cards = [
     might: 376,
     description: 'The leader of the Horse Clan, one of the Twelve Wyrmclans who govern Hinomoto. Replete with determination, Nobunaga longs to become shogun and unite the country beneath her, and will cut down any who get in her way.',
     image: "https://gamepress.gg/dragalialost/sites/dragalialost/files/2019-12/110346_01_r05.png",
-    position: "-19em -12em",
+    position: "-19em -10em",
     theme: "red-theme"
+  },
+  {
+    id: 4,
+    name: 'Lin You',
+    might: 289,
+    description: 'A young woman who trained with the Qilin tribe of Taiwu. She can be a little naive, but has firm beliefs when it comes to competitions and improving her martial-arts skills, and burns with a desire to become as strong as possible.',
+    image: "https://gamepress.gg/dragalialost/sites/dragalialost/files/2019-01/Lin%20You-5-star.png",
+    position: "-25em -7em",
+    theme: "pink-theme"
   }
 ];
